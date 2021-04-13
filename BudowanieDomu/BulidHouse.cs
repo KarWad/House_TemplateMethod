b48e26283ddd958463d265f9e87427cb0216e068
KarWad <phinb@LAPTOP-25LHGEP6.(none)>
2021-04-13 13:41:18 +0200

    Dodaj pliki projektów.

---
+++ BudowanieDomu/BulidHouse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TemplateMethodDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Build a Concrete House\n");
+            HouseTemplate houseTemplate = new ConcreteHouse();
+            houseTemplate.BuildHouse();
+            Console.WriteLine();
+            Console.WriteLine("Build a Wooden House\n");
+            houseTemplate = new WoodenHouse();
+            houseTemplate.BuildHouse();
+            Console.Read();
+        }
+    }
+}
